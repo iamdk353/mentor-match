1461c5dc02c4e0ab9ea1e76e0be4b0c3225da2cd
Dhanush Kumar M <iamdk353@gmail.com>
2024-12-20 04:22:19 +0530

    profile restart

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,24 +1,4 @@
-"use client";
-import ProfileCard from "@/components/profile";
-import ProfileForm from "@/components/profileForm";
-import { useUser } from "@clerk/nextjs";
-import { Edit } from "lucide-react";
-import { useState } from "react";
-
 const page = () => {
-  const { user } = useUser();
-  const [edit, setEdit] = useState(false);
-  return (
-    <>
-      {edit ? (
-        <ProfileForm setEdit={setEdit} />
-      ) : (
-        // <p>hello</p>
-        <>
-          <ProfileCard setEdit={setEdit} />
-        </>
-      )}
-    </>
-  );
+  return <div>page</div>;
 };
 export default page;
